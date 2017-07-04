21de90cc89a735c88e99344a732f2b5d06e474bb
Aubrey Howell <simplyaubs@gmail.com>
2017-07-04 10:29:49 -0500

    working on TDD

--- spec/features/cities_spec.rb
+++ spec/features/cities_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 feature 'CRUD favorites cities' do
   scenario 'create a list of cities' do
     visit '/'
+    fill_in "city", with: "Chicago"
+    fill_in "state", with: "IL"
 
+  end
   end
 end
